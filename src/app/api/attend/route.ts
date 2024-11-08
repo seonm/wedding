ffed321a787fbf3c3f7b751b3a606dedff076568
seonm <misoda1004@gmail.com>
2024-11-08 23:53:42 +0900

    next api cors

--- src/app/api/attend/route.ts
+++ src/app/api/attend/route.ts
@@ -35,7 +35,17 @@ export async function POST(req: Request) {
     });
 
     await newAttend.save();
-    return NextResponse.json(newAttend.toJSON(), { status: 201 });
+
+    const response = NextResponse.json(
+      { message: 'User added successfully' },
+      { status: 201 }
+    );
+    response.headers.set('Access-Control-Allow-Origin', '*'); // 모든 도메인 허용
+    response.headers.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS'); // 허용된 메서드
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type'); // 허용된 헤더
+
+    // return NextResponse.json(newAttend.toJSON(), { status: 201 });
+    return response;
   } catch (error) {
     return NextResponse.json(
       { error: `Failed to fetch users${error}` },
