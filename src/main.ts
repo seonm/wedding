296be6578be256f1b44dc4661c5f3e304bbe6384
seonm <misoda1004@gmail.com>
2024-11-18 22:07:45 +0900

    cors 허용 2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'UPDATE'] });
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+  app.use((req: Request, res: Response, next: Function) => {
+    if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      );
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization',
+      );
+      res.status(204).send();
+    } else {
+      next();
+    }
+  });
 
   await app.listen(3000);
 }
